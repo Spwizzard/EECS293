dfc55f5dbbfa3a0f65014a0667fa8aa9a33204ac
Steven Leonis <Spwizzard2@gmail.com>
2017-09-18 21:36:13 -0400

    updates from discussion 2

--- src/lexer/Token.java
+++ src/lexer/Token.java
@@ -10,11 +10,11 @@ public final class Token {
 		NOT 		("not", false),
 		AND 		("and", false),
 		OR			("or", false),
-		OPEN 		("(", false),
-		CLOSE 		(")", false),
+		OPEN 		("\\(", false),
+		CLOSE 		("\\)", false),
 		ID 			("[a-z]+", true), //a string of lower case letters
-		NUMBER 		("([-])?\\d+", true), //a sequence of digits, with a possible - sign
-		BINARYOP 	("[+-*/]", true), //one of +,-,*, or /
+		NUMBER 		("-?\\d+", true), //a sequence of digits, with a possible - sign
+		BINARYOP 	("[\\+\\-\\*\\/]", true), //one of +,-,*, or /
 		WHITESPACE 	("\\s+", false); //a sequence of whitespace
 		
 		private final String pattern;
@@ -49,17 +49,34 @@ public final class Token {
 		}
 		
 		@Override
-		public boolean equals(Object obj) {
-			// TODO Auto-generated method stub
-			return super.equals(obj);
-		}
-		@Override
 		public int hashCode() {
-			// TODO Auto-generated method stub
-			return super.hashCode();
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((data == null) ? 0 : data.hashCode());
+			result = prime * result + ((type == null) ? 0 : type.hashCode());
+			return result;
 		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Builder other = (Builder) obj;
+			if (data == null) {
+				if (other.data != null)
+					return false;
+			} else if (!data.equals(other.data))
+				return false;
+			if (type != other.type)
+				return false;
+			return true;
+		}
+		
 	}
-	
 	
 	private final Type type;
 	private final Optional<String> data;
@@ -85,38 +102,51 @@ public final class Token {
 			 builder = new Builder(type, Optional.ofNullable(data));
 		}
 		else{
-			builder = new Builder(type, Optional.empty());
+			builder = new Builder(type, Optional.<String>empty());
 		}
 		
 		//now check if that builder already exists in the map		
-		if(tokenMap.containsKey(builder)){
-			//the token already exists, ignore the new one we made
-		}
-		else{
+		if(!tokenMap.containsKey(builder)){
 			//the token doesn't already exist, add it to the map
 			tokenMap.put(builder, builder.build());
 		}
+		//else case: the token already exists, ignore the new one we made
 		
 		//either way we want to return the token that results from the builder
 		return tokenMap.get(builder);
 	}
 	
 	@Override
-	public boolean equals(Object obj) {
-		// TODO Auto-generated method stub
-		return super.equals(obj);
-	}
-	@Override
 	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
 	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Token other = (Token) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
+		return "Token [type=" + type + ", data=" + data + "]";
 	}
-	
-	
 	
 }
